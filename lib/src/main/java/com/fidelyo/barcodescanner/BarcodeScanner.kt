b756c864e1c1470d3632a40f4255a1062af5e932
bishoy <bishoybasily@gmail.com>
2017-12-29 14:11:02 +0200

    dm

--- lib/src/main/java/com/fidelyo/barcodescanner/BarcodeScanner.kt
+++ lib/src/main/java/com/fidelyo/barcodescanner/BarcodeScanner.kt
@@ -1,11 +1,8 @@
 package com.fidelyo.barcodescanner
 
 import android.app.Activity
-import android.app.Fragment
 import android.content.Intent
-import android.os.Bundle
 import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
 
 /**
  * Created by bishoy on 12/26/17.
@@ -30,36 +27,6 @@ class BarcodeScanner {
             fragmentManager.executePendingTransactions()
         }
         return fragment as BarcodeScannerFragment
-    }
-
-    class BarcodeScannerFragment : Fragment() {
-
-        private var emitter: ObservableEmitter<Code>? = null
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            retainInstance = true
-        }
-
-        fun setEmitter(emitter: ObservableEmitter<Code>): BarcodeScannerFragment {
-            this.emitter = emitter
-            return this
-        }
-
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            super.onActivityResult(requestCode, resultCode, data)
-            if (resultCode == Activity.RESULT_OK) {
-                if (requestCode == BarcodeScanner.CODE) {
-                    if (data != null) {
-                        if (emitter != null) {
-                            emitter!!.onNext(data.getSerializableExtra(EXTRA) as Code)
-                            emitter!!.onComplete()
-                        }
-                    }
-                }
-            }
-        }
-
     }
 
     companion object {
---
+++ lib/src/main/java/com/fidelyo/barcodescanner/BarcodeScannerFragment.kt
@@ -0,0 +1,37 @@
+package com.fidelyo.barcodescanner
+
+import android.app.Activity
+import android.app.Fragment
+import android.content.Intent
+import android.os.Bundle
+import io.reactivex.ObservableEmitter
+
+class BarcodeScannerFragment : Fragment() {
+
+    private var emitter: ObservableEmitter<Code>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    fun setEmitter(emitter: ObservableEmitter<Code>): BarcodeScannerFragment {
+        this.emitter = emitter
+        return this
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == BarcodeScanner.CODE) {
+                if (data != null) {
+                    if (emitter != null) {
+                        emitter!!.onNext(data.getSerializableExtra(BarcodeScanner.EXTRA) as Code)
+                        emitter!!.onComplete()
+                    }
+                }
+            }
+        }
+    }
+
+}
